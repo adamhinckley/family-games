056ac4c19bdd01a4e668f3a351c1c281f33566fe
Adam Hinckley <adamhinckley@mac.com>
2019-12-10 23:40:21 -0700

    applies max width to the app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React from "react";
 import "./App.css";
 import Twister from "./components/twister";
-// eslint-disable-next-line
-import { Route, Switch, withRouter, Redirect } from "react-router-dom";
-import Home from "./components/home";
+import { Route, Switch, withRouter } from "react-router-dom";
+// import Home from "./components/home";
 import bingo from "./components/bingo";
+import styled from "styled-components";
 
+const Container = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+`;
 function App() {
   return (
     // <div className="App">
     //   <Twister />
     //   <Footer />
     // </div>
-    <Switch>
-      <Route exact path="/" component={bingo} />
-      <Route exact path="/twister" component={Twister} />
-      <Route exact path="/bingo" component={bingo} />
-    </Switch>
+    <Container>
+      <Switch>
+        <Route exact path="/" component={bingo} />
+        <Route exact path="/twister" component={Twister} />
+        <Route exact path="/bingo" component={bingo} />
+      </Switch>
+    </Container>
   );
 }
 
